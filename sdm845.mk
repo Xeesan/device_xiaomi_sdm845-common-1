554488e233829886991da0bc49ca205b994ceb2b
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    sdm845-common: Switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Bug: 69270928
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- sdm845.mk
+++ sdm845.mk
@@ -312,11 +312,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
-    android.hardware.power-impl
+    android.hardware.power@1.2-service.xiaomi_sdm845-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # PocoPref settigs modules
 #PRODUCT_PACKAGES += \
